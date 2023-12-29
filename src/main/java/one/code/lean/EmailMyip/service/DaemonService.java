cefa99c07878b0abf453fb75602cbeee652ab7cd
Leandro-Aballone <l.aballone@gmail.com>
2023-12-29 00:05:55 -0300

    Added DaemonService

--- src/main/java/one/code/lean/EmailMyip/service/DaemonService.java
+++ src/main/java/one/code/lean/EmailMyip/service/DaemonService.java
@@ -1,2 +1,18 @@
-package one.code.lean.EmailMyip.service;public class DaemonService {
+package one.code.lean.EmailMyip.service;
+
+import lombok.AllArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import java.util.Timer;
+
+@Service
+@AllArgsConstructor
+public class DaemonService {
+    @Autowired
+    private CheckIpService checkIpService;
+    public String startIpChecker(){
+        Timer timer = new Timer();
+        timer.schedule(checkIpService,0,60000L * 10);
+        return "Started.";
+    }
 }
